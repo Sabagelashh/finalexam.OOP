a9b8d712e765487318f0a27093adca90dad9fe8d
Sabagelashh <147238067+Sabagelashh@users.noreply.github.com>
2024-07-01 09:33:23 +0400

    Delete finalexam directory

--- finalexam/task4/LegalEntity.java
+++
@@ -1,6 +0,0 @@
-package finalexam.task4;
-
-public interface LegalEntity {
-    String getAddress();
-    String getVatNumber();
-}
--- finalexam/task5/CommunicationManagerTester.java
+++
@@ -1,9 +0,0 @@
-package finalexam.task5;
-
-public class CommunicationManagerTester {
-    public static void main(String[] args) {
-        CommunicationManager manager = new CommunicationManager("https://example.com/api/chatbot");
-        String response = manager.sendUserMessage("Hello, chatbot!");
-        System.out.println("Response: " + response);
-    }
-}
